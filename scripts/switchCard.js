75e18a0d76569cc9aa66008e24612b1d28bbce4a
YongLeeCode <yonghwan311@gmail.com>
2025-02-20 14:27:18 +0900

    fix: 디테일페이지 ui 수정

--- scripts/switchCard.js
+++ scripts/switchCard.js
@@ -6,28 +6,19 @@ function updateCard() {
 	const cardCase = document.getElementById("cardCase");
 	const currentData = window.sharedData[currentIndex];
 	console.log(currentData);
-	cardCase.innerHTML = `
-    				<img
-					src="${currentData.image}"
-					id="card-image"
-					alt="MBTI"
-				/>
-				<div id="card-body">
-					<p id="card-title">${currentData.title}</p>
-					<p id="card-text">
-						${currentData.content}
-					</p>
-        </div>
-    `;
+
+	document.getElementById("card-image").src = currentData.image;
+	document.getElementById("card-title").innerText = currentData.title;
+	document.getElementById("card-text").innerText = currentData.content;
 }
 
-document.getElementById('prevBtn').addEventListener('click', () => {
-  if (!window.sharedData) return;
-  if (currentIndex === 0) {
-    currentIndex = window.sharedData.length - 1;
-  }
-  currentIndex = (currentIndex - 1) % window.sharedData.length;
-  updateCard();
+document.getElementById("prevBtn").addEventListener("click", () => {
+	if (!window.sharedData) return;
+	if (currentIndex === 0) {
+		currentIndex = window.sharedData.length - 1;
+	}
+	currentIndex = (currentIndex - 1) % window.sharedData.length;
+	updateCard();
 });
 
 document.getElementById("nextBtn").addEventListener("click", () => {
